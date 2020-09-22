cca7b380cd3cb85933f070e6478237f7b04bdab0
Jakub Aniszewski <qba.anisz@gmail.com>
2020-09-22 15:55:10 +0200

    Install vuetify

--- ems-frontend/src/App.vue
+++ ems-frontend/src/App.vue
@@ -1,30 +1,55 @@
 <template>
-  <div id="app">
-    <Navbar />
-    <!-- <router-view /> -->
-  </div>
+  <v-app>
+    <v-app-bar app color="secondary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import Navbar from "./components/layout/Navbar";
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: "app",
+  name: 'App',
+
   components: {
-    Navbar
-  }
+    HelloWorld
+  },
+
+  data: () => ({
+    //
+  })
 };
 </script>
-
-<style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
-}
-</style>
