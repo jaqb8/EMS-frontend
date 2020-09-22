091bf7af0bbff7f1b4134cdafb01fadc84075860
Michał Niżyński <mic.nizynski@gmail.com>
2020-09-22 22:48:58 +0200

    create login form

---
+++ ems-frontend/src/components/register.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login-form">
+    <div class="form-top-bar">
+      <div class="bar-top-line"></div>
+      <div class="bar-bottom-container">
+        <h1>EMS</h1>
+        <p>Login</p>
+      </div>
+    </div>
+    <form>
+      <input id="email" type="text">
+      <input id="password" type="password">
+      <input id="retype-password" type="password">
+      <button id="form-submit-btn">Login</button>
+    </form>
+    <p class="create-account">Or create an account if you don’t have it yet.
+You can do it here.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data (){
+    return {
+      msg: 'Welcome'
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ ems-frontend/src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="login">
+    <Login/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from "@/components/login.vue";
+import axios from "axios";
+
+export default {
+  name: "login",
+  methods: {
+    async login(){
+      const data = await axios.post("http://localhost:5001/ems-employee-managment-system/us-central1/app/api/users", 
+      {
+        email: "stolec@gmail.com",
+        password: "xdxdxd",
+        department: "Aspergers",
+        position: "Aspi"
+      }, 
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      console.log(data);
+      return data;
+    }
+  },
+  components: {
+    Login
+  }
+};
+</script>
